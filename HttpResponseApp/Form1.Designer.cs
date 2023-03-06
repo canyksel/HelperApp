7945e0b5f4ab97e2b4551399a2a228db551d8325
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-03-06 23:07:14 +0300

    feat: little update.

--- HttpResponseApp/Form1.Designer.cs
+++ HttpResponseApp/Form1.Designer.cs
@@ -221,7 +221,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(182, 23);
             this.button2.TabIndex = 16;
-            this.button2.Text = "Stop Scan";
+            this.button2.Text = "Stop Analyze";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -244,7 +244,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(182, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Start Scan";
+            this.button1.Text = "Start Analyze";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
